9c748e92ccd052ad4fa7bb08b6827826de468536
brentschmaltz <brentschmaltz@hotmail.com>
2022-12-26 16:54:15 -0800

    stringA

--- CodeSamples/BasicCLR/Dictionaries.cs
+++ CodeSamples/BasicCLR/Dictionaries.cs
@@ -5,7 +5,7 @@ namespace CodeSnips.BasicCLR
 {
     public class Dictionaries
     {
-        private static Dictionary<string, object> _dictionary = new Dictionary<string, object> { { "1", "stringB" } };
+        private static Dictionary<string, object> _dictionary = new Dictionary<string, object> { { "1", "stringA" } };
 
         public static void Run()
         {
